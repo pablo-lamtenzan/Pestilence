e57beadcc4b0a13ac5cf5f2e2b966ca93e9218c0
plamtenz <plamtenz@student.le-101.fr>
2020-02-29 22:27:36 +0100

    [FUTURE OFUSCATION LIB]

---
+++ __future__/ofuscation.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ofuscation.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plamtenz <plamtenz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/29 21:36:11 by plamtenz          #+#    #+#             */
+/*   Updated: 2020/02/29 22:25:33 by plamtenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OFUSCATION_H
+# define OFUSCATION_H
+
+/*
+**
+**      --> ANTI-DEBUG : PESTILENCE
+**
+*/
+#include <../anti-debug/includes/anti_debug.h> // #include <anti_debug>
+
+/*
+**
+**
+**      --> POLIMORPHIC CODE : WAR
+**
+*/
+
+// TO DO 
+
+/*
+**
+**      --> METAMORPHIC CODE : DEATH
+**
+*/
+
+// TO DO
+
+
+/* I know probally i have to re-write all for war or death buy tried to code this project all the modular as posible*/
+
+#endif
